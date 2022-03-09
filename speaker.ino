aaecee929dc6b6f5020d84482e4196defe5d0475
alifareeq77 <alifareeq77@gmail.com>
2022-03-09 18:46:05 +0300

    fixing serial monitior

--- speaker.ino
+++ speaker.ino
@@ -1,6 +1,6 @@
 void setup() {
   Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
-  pinMode(13,OUTPUT);
+  pinMode(13,OUTPUT); // use led because we can't use serial port in both IDE
 
 }
 
@@ -10,9 +10,6 @@ void loop() {
   if (Serial.available() > 0) {
     // read the incoming byte:
    unsigned  int  incomingByte = Serial.read();
-
-    // say what you got:
-    
     digitalWrite(13,HIGH);
     Serial.print("I received: ");
     Serial.println(incomingByte, DEC);
